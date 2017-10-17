7ef82c107ed2fc19fb2926edd636ee6260b884d5
pennzht <penn.zht@gmail.com>
2017-10-16 22:16:00 -0700

    Created Toolbox

--- home.js
+++ home.js
@@ -1,8 +1,14 @@
 function init(){
-    blocks = document.body.getElementsByClassName('block');
-    for(i in blocks){
-        var block = blocks[i];
-        setDrag(block);
+    sidebar = document.body.getElementsByClassName('sidebar')[0];
+    blocks = []
+    lines = ["Shall I compare thee to a summer's day?", "Thou art more lovely and more temperate.", "Rough winds do shake the darling buds of May,", "And summer's lease hath all too short a date.", "Sometime too hot the eye of heaven shines,", "And often is his gold complexion dimmed;", "And every fair from fair sometime declines,", "By chance, or nature's changing course, untrimmed;", "But thy eternal summer shall not fade,", "Nor lose possession of that fair thou ow'st,", "Nor shall death brag thou wand'rest in his shade,", "When in eternal lines to Time thou grow'st.", "So long as men can breathe, or eyes can see,", "So long lives this, and this gives life to thee."];
+    var nblock, ntext;
+    for(i in lines){
+        nblock = document.createElement("div");
+        ntext = document.createTextNode(lines[i]);
+        nblock.setAttribute("class", "demoblock");
+        nblock.appendChild(ntext);
+        sidebar.appendChild(nblock);
     }
 }
 
@@ -12,13 +18,14 @@ function setDrag(block){
     function dragMouseDown(e){
         xstart = e.clientX;
         ystart = e.clientY;
-        block.onmouseup = dragMouseUp;
-        block.onmousemove = dragMouseMove;
+        document.onmouseup = dragMouseUp;
+        // block.onmouseleave = dragMouseUp;
+        document.onmousemove = dragMouseMove;
     }
 
     function dragMouseUp(e){
-        block.onmouseup = null;
-        block.onmousemove = null;
+        document.onmouseup = null;
+        document.onmousemove = null;
     }
 
     function dragMouseMove(e){
